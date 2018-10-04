7bf4c7a6cb72c16954788f36cf834a56114b59cf
aubreeabril <aubree.abril@gmail.com>
2018-10-04 10:46:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,60 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import logo from "./logo.svg";
+import "./App.css";
+import { createStore } from "redux";
+
+/* how to update over time */
+// 1. What is the shape of our state?
+// { count: number, friends: []string, inputText: string, currentUser: { name: string, age: number } }
+// 2. initial values
+// 3. how the values change
+
+// 1. shape { count: number }
+// 2. initial values { count: 0 }
+// 3. changes over time (how does it respond to different actions?):
+//    increment action => count should go up by 1
+//    decrement action => down by 1
+
+const reducer = (oldState, action) => {
+  console.log(oldState, action);
+  if (oldState == undefined) {
+    let initialState = { count: 50 };
+    return initialState;
+  }
+  let updatedState = oldState;
+  // we don't do:
+  // BADDDDD !!!!! oldState.count += 1
+
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: oldState.count + action.value };
+    case "DECREMENT":
+      return { count: oldState.count - action.value };
+    case "RESET":
+      return { count: 0 };
+    default:
+      return updatedState;
+  }
+};
+
+// const reducer = (oldState = { count: 0 }, action) => {
+// return oldState;
+// };
+
+const store = createStore(reducer /* other options */);
+
+console.log("the state is", store.getState());
 
 class App extends Component {
+  componentDidMount() {
+    // WARNING: HACK!!!!
+    store.subscribe(() => {
+      this.forceUpdate();
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,43 +66,51 @@ class App extends Component {
 }
 
 class Header extends Component {
+  renderDescription = () => {
+    const remainder = store.getState().count % 5;
+    const upToNext = 5 - remainder;
+    return `The current count is less than ${store.getState().count +
+      upToNext}`;
+  };
+
   render() {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
+        <h3>{this.renderDescription()}</h3>
       </header>
     );
   }
 }
 
 class Counter extends Component {
-  state = { count: 0 };
-
-  increment = () => {
-    this.setState(prevState => ({ count: prevState.count + 1 }));
+  increment = value => {
+    // this.setState(prevState => ({ count: prevState.count + 1 }));
+    store.dispatch({ type: "INCREMENT", value });
   };
 
-  decrement = () => {
-    this.setState(prevState => ({ count: prevState.count - 1 }));
+  decrement = value => {
+    store.dispatch({ type: "DECREMENT", value });
+    // this.setState(prevState => ({ count: prevState.count - 1 }));
   };
 
-  renderDescription = () => {
-    const remainder = this.state.count % 5;
-    const upToNext = 5 - remainder;
-    return `The current count is less than ${this.state.count + upToNext}`;
+  reset = () => {
+    store.dispatch({ type: "RESET" });
   };
 
   render() {
     return (
       <div className="Counter">
-        <h1>{this.state.count}</h1>
-        <button onClick={this.decrement}> - </button>
-        <button onClick={this.increment}> + </button>
-        <h3>{this.renderDescription()}</h3>
+        <h1>{store.getState().count}</h1>
+        <button onClick={() => this.decrement(1)}> - </button>
+        <button onClick={() => this.increment(1)}> + </button>
+        <button onClick={() => this.increment(3)}> +3 </button>
+        <button onClick={() => this.decrement(10)}> -10 </button>
+        <button onClick={this.reset}> RESET </button>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
